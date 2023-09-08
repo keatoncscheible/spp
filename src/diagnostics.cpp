7796569ba6c9aa1b8cbe8047893c3ece8fee8e44
Keaton Scheible <keatoncscheible@gmail.com>
2023-09-08 12:13:11 -0700

    improve formatting

--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -20,31 +20,31 @@ void* task_diagnostics(void* arg) {
     pthread_cond_t cond;
     pthread_cond_init(&cond, nullptr);  // Initialize condition variable
 
-    std::ofstream logFile("diagnostics/diagnostics_out.txt");
+    std::ofstream log_file("diagnostics/diagnostics_out.txt");
 
     while (true) {
         pthread_mutex_lock(&mutex);
 
         // Calculate the absolute time for timedwait
-        struct timespec absTime;
-        clock_gettime(CLOCK_REALTIME, &absTime);
-        absTime.tv_sec += delay.tv_sec;
-        absTime.tv_nsec += delay.tv_nsec;
-        if (absTime.tv_nsec >= 1000000000) {
-            absTime.tv_sec++;
-            absTime.tv_nsec -= 1000000000;
+        struct timespec abs_time;
+        clock_gettime(CLOCK_REALTIME, &abs_time);
+        abs_time.tv_sec += delay.tv_sec;
+        abs_time.tv_nsec += delay.tv_nsec;
+        if (abs_time.tv_nsec >= 1000000000) {
+            abs_time.tv_sec++;
+            abs_time.tv_nsec -= 1000000000;
         }
 
         // Wait for the specified time or until signaled
-        pthread_cond_timedwait(&cond, &mutex, &absTime);
+        pthread_cond_timedwait(&cond, &mutex, &abs_time);
 
-        logFile << "Diagnostics" << std::endl;
-        logFile.flush();  // Flush the buffer to ensure data is written
+        log_file << "Diagnostics" << std::endl;
+        log_file.flush();  // Flush the buffer to ensure data is written
 
         pthread_mutex_unlock(&mutex);
     }
 
-    logFile.close();
+    log_file.close();
     pthread_mutex_destroy(&mutex);  // Clean up mutex
     pthread_cond_destroy(&cond);    // Clean up condition variable
 
